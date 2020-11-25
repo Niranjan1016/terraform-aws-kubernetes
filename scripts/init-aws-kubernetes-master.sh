0c48091c49a3cd98d78a7cffbb54777c005663fd
Niranjan1016 <niranjanputta@gmail.com>
2020-11-25 17:49:17 -0500

    Update init-aws-kubernetes-master.sh

--- scripts/init-aws-kubernetes-master.sh
+++ scripts/init-aws-kubernetes-master.sh
@@ -28,30 +28,32 @@ FULL_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/hostname)"
 DNS_NAME=$(echo "$DNS_NAME" | tr 'A-Z' 'a-z')
 
 # Install AWS CLI client
-yum install -y epel-release
-yum install -y python2-pip
-pip install awscli --upgrade
+#ap install -y python2-pip
+#pip install awscli --upgrade
 
 # Tag subnets
-for SUBNET in $AWS_SUBNETS
-do
-  aws ec2 create-tags --resources $SUBNET --tags Key=kubernetes.io/cluster/$CLUSTER_NAME,Value=shared --region $AWS_REGION
-done
+#for SUBNET in $AWS_SUBNETS
+#do
+#  aws ec2 create-tags --resources $SUBNET --tags Key=kubernetes.io/cluster/$CLUSTER_NAME,Value=shared --region $AWS_REGION
+#done
 
 # Install docker
-yum install -y yum-utils device-mapper-persistent-data lvm2 docker
+apt-get update & apt-get install -y device-mapper-persistent-data lvm2 docker
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
 
 # Install Kubernetes components
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+# sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+# [kubernetes]
+# name=Kubernetes
+# baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+# enabled=1
+# gpgcheck=1
+# repo_gpgcheck=1
+# gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+#         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# EOF
 
 # setenforce returns non zero if already SE Linux is already disabled
 is_enforced=$(getenforce)
@@ -61,7 +63,7 @@ if [[ $is_enforced != "Disabled" ]]; then
   
 fi
 
-yum install -y kubelet-$KUBERNETES_VERSION kubeadm-$KUBERNETES_VERSION kubernetes-cni
+apt-get install -y kubelet-$KUBERNETES_VERSION kubeadm-$KUBERNETES_VERSION kubernetes-cni
 
 # Start services
 systemctl enable docker
@@ -74,7 +76,7 @@ sysctl net.bridge.bridge-nf-call-iptables=1
 sysctl net.bridge.bridge-nf-call-ip6tables=1
 
 # Fix certificates file on CentOS
-if cat /etc/*release | grep ^NAME= | grep CentOS ; then
+if cat /etc/*release | grep ^NAME= | grep Ubuntu ; then
     rm -rf /etc/ssl/certs/ca-certificates.crt/
     cp /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
 fi
@@ -144,19 +146,19 @@ kubectl apply -f /tmp/calico.yaml
 # Allow the user to administer the cluster
 kubectl create clusterrolebinding admin-cluster-binding --clusterrole=cluster-admin --user=admin
 
-# Prepare the kubectl config file for download to client (IP address)
-export KUBECONFIG_OUTPUT=/home/centos/kubeconfig_ip
+# Prepare the kubectl config file for download to cient (IP address)
+export KUBECONFIG_OUTPUT=/home/ubuntu/kubeconfig_ip
 kubeadm alpha kubeconfig user \
   --client-name admin \
   --apiserver-advertise-address $IP_ADDRESS \
   > $KUBECONFIG_OUTPUT
-chown centos:centos $KUBECONFIG_OUTPUT
+chown ubuntu:ubuntu $KUBECONFIG_OUTPUT
 chmod 0600 $KUBECONFIG_OUTPUT
 
-cp /home/centos/kubeconfig_ip /home/centos/kubeconfig
-sed -i "s/server: https:\/\/$IP_ADDRESS:6443/server: https:\/\/$DNS_NAME:6443/g" /home/centos/kubeconfig
-chown centos:centos /home/centos/kubeconfig
-chmod 0600 /home/centos/kubeconfig
+cp /home/ubuntu/kubeconfig_ip /home/ubuntu/kubeconfig
+sed -i "s/server: https:\/\/$IP_ADDRESS:6443/server: https:\/\/$DNS_NAME:6443/g" /home/ubuntu/kubeconfig
+chown ubuntu:ubuntu /home/ubuntu/kubeconfig
+chmod 0600 /home/ubuntu/kubeconfig
 
 # Load addons
 for ADDON in $ADDONS
